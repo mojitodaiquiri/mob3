b95496095ab8ec890e7ab152b73c787681e097b3
mojitodaiquiri <mojitodaiquiri@gmail.com>
2013-09-17 09:53:29 +0200

    primo rilascio

---
+++ js/logic.js
@@ -0,0 +1,49 @@
+	
+	
+	var server;
+	var username;
+	var password;
+	
+	$(document).on('pageinit', '#home', function() {
+		//alert("ciao sono home");
+		server = localStorage.getItem('server');
+		username = localStorage.getItem('username');
+		password = localStorage.getItem('password');
+		
+		if (server == "" || username == "" || password == "")
+		{
+			// cambio alla pagina option
+			$.mobile.changePage( $("#option"), "slide", true, true);
+		}
+		
+	});
+	
+	$(document).on('pageinit', '#option', function() {
+		//alert("ciao sono option");
+		
+		$('#txtUrl').val(server);
+		$('#txtUsername').val(username);
+		$('#txtPassword').val(password);
+
+			
+			
+	});
+
+	
+	$('#btnSave').click(function() {
+		server = $('#txtUrl').val();
+		username = $('#txtUsername').val();
+		password = $('#txtPassword').val();
+		if (server != "" && username != "" && password != "")
+		{
+			localStorage.setItem('server',server);
+			localStorage.setItem('username',username);
+			localStorage.setItem('password',password);
+			alert('Saved');
+		}
+		else
+		{
+			alert("fill all fields!")
+		}
+	    
+	});
---
+++ js/personalizzazioni.js
@@ -0,0 +1,6 @@
+$(document).bind("mobileinit", function(){
+	$.mobile.listview.prototype.options.filterPlaceholder= "Filtro ...";
+    $.mobile.page.prototype.options.backBtnText = "Indietro";
+  
+});
+
